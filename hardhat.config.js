ae8c1d2cc65470d3d1f0017db04ce8ce162661a2
7i7o <mparij@gmail.com>
2022-08-04 01:14:09 -0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.12",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 10,
+      },
+    },
+  },
+};
+
+// 01 Mumbai: 0x8870A775491580Dca0cf540fD2236b23d1655003
